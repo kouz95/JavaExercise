c200bff0bf3eec9a0dcaec470ce985eb4b3759f5
kouz <lastpuzly@gmail.com>
2019-11-30 22:22:16 +0900

    feat: GamerDraw기능 구현

---
+++ BlackJack/src/main/java/domain/Gamer.java
@@ -0,0 +1,15 @@
+package main.java.domain;
+
+import java.util.ArrayList;
+
+public class Gamer {
+    private ArrayList<Pair<String, Integer>> cardsPool;
+
+    public Gamer() {
+        cardsPool = new ArrayList<>();
+    }
+
+    public void drawCard(ArrayList<Pair<String, Integer>> deck) {
+        cardsPool.add(CardHandler.DrawCard(deck));
+    }
+}
