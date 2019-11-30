19ad97b14a4939c63a61e144041bf282c7461c82
kouz <lastpuzly@gmail.com>
2019-11-30 22:32:54 +0900

    feat: 점수 계산 기능 구현

---
+++ BlackJack/src/main/java/domain/ScoreCalculator.java
@@ -0,0 +1,14 @@
+package main.java.domain;
+
+import java.util.ArrayList;
+
+public class ScoreCalculator {
+    public static int getScore(ArrayList<Pair<String, Integer>> cardsPool) {
+        int score = 0;
+
+        for (Pair<String, Integer> card : cardsPool) {
+            score += card.getRight();
+        }
+        return score;
+    }
+}
