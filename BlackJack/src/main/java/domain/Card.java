930d9cba2542784ec58e098572f84d104cc8a11c
kouz <lastpuzly@gmail.com>
2019-11-30 22:15:39 +0900

    refactor: 오타 수정

--- BlackJack/src/main/java/domain/Card.java
+++ BlackJack/src/main/java/domain/Card.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 
 public class Card {
     private static final int TOTAL_NUM = 52;
-    private static final int SAME_PATTREN_NUM = 13;
+    private static final int SAME_PATTERN_NUM = 13;
     private static final int MAX_VALUE_OF_NUM = 10;
     private static final String[] PATTERN = {"SPADE", "DIAMOND", "HEART", "CLOVER"};
 
@@ -13,6 +13,10 @@ public class Card {
 
     public Card() {
         Collections.shuffle(deckList);
+    }
+
+    public ArrayList<Pair> getDeckList() {
+        return deckList;
     }
 
     private ArrayList<Pair> setDefalutDeck() {
@@ -24,9 +28,9 @@ public class Card {
     }
 
     private void setPatternOf(ArrayList<Pair> deck, String pattern) {
-        for(int i = 1 ; i <= SAME_PATTREN_NUM ; i++) {
+        for(int i = 1 ; i <= SAME_PATTERN_NUM ; i++) {
             if(i >= MAX_VALUE_OF_NUM) {
-                deck.add(Pair.of(pattern,MAX_VALUE_OF_NUM));
+                deck.add(Pair.of(pattern, MAX_VALUE_OF_NUM));
             }
             deck.add(Pair.of(pattern, i));
         }
