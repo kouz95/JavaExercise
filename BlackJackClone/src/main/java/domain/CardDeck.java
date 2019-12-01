02ade43826c79c8605a84521b300ce86203684f2
kouz <lastpuzly@gmail.com>
2019-12-01 16:28:36 +0900

    docs: 느낀점 업데이트

---
+++ BlackJackClone/src/main/java/domain/CardDeck.java
@@ -0,0 +1,11 @@
+package main.java.domain;
+
+import java.util.List;
+
+public class CardDeck {
+    private List<Card> cards;
+
+    public Card getCard() {
+        return null;
+    }
+}
---
+++ BlackJackClone/src/main/java/domain/Gamer.java
@@ -0,0 +1,13 @@
+package main.java.domain;
+
+import java.util.List;
+
+public class Gamer {
+    private List<Card> cards;
+
+    public void receiveCard(Card card) {}
+
+    public List<Card> openCards() {
+        return null;
+    }
+}
