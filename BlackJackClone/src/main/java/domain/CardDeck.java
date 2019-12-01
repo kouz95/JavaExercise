302c8c6f88fe5b2eba2485cba49e847e0ea6b848
kouz <lastpuzly@gmail.com>
2019-12-01 16:42:34 +0900

    생성자를 사용하는 타이밍

--- BlackJackClone/src/main/java/domain/CardDeck.java
+++ BlackJackClone/src/main/java/domain/CardDeck.java
@@ -1,5 +1,6 @@
 package main.java.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CardDeck {
@@ -7,6 +8,33 @@ public class CardDeck {
     private static final int CARD_COUNT = 13;
 
     private List<Card> cards;
+
+    public CardDeck() {
+        cards = new ArrayList<>();
+
+        for(String pattern : PATTERNS){
+            for(int i=1; i<=CARD_COUNT; i++) {
+                Card card = new Card();
+                String denomination;
+
+                if(i == 1){
+                    denomination = "A";
+                }else if(i == 11){
+                    denomination = "J";
+                }else if(i == 12){
+                    denomination = "Q";
+                }else if(i == 13){
+                    denomination = "K";
+                }else {
+                    denomination = String.valueOf(i);
+                }
+
+                card.setDenomination(denomination);
+                card.setPattern(pattern);
+                cards.add(card);
+            }
+        }
+    }
 
     public Card getCard() {
         return null;
