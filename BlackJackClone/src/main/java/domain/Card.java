d0b6937c31546991794fcd00e0ca86af3f9c68ef
kouz <lastpuzly@gmail.com>
2019-12-01 16:22:18 +0900

    docs: 느낀점 업데이트

---
+++ BlackJackClone/src/main/java/domain/Card.java
@@ -0,0 +1,22 @@
+package main.java.domain;
+
+public class Card {
+    private String pattern;
+    private String denomination;
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getDenomination() {
+        return denomination;
+    }
+
+    public void setDenomination(String denomination) {
+        this.denomination = denomination;
+    }
+}
